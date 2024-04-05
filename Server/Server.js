c1fe8d070ac5fbad13f813dbfb9bef4954bd8b3b
michaspie8 <michaspie8@gmail.com>
2024-04-05 11:31:11 +0200

    Complete categorization system

--- Server/Server.js
+++ Server/Server.js
@@ -7,7 +7,13 @@ const mysql = require("mysql");
 //openai client
 const openaiModule = require("openai");
 const openai = new openaiModule("sk-xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx");
-
+const host = {
+    hostname: "localhost",
+    port: process.env.PORT || "3000",
+}
+function fullhostname(){
+    return this.hostname+":"+this.port;
+}
 
 const connection = mysql.createConnection({
   host: "localhost:3306",
@@ -45,8 +51,28 @@ connection.query(
     }
 );
 
+app.get("/test", (req, res) => {
+    const headers = {
+        "Content-Type": "application/json"
+    }
+   //fetch() wypadek, with example data, audio from nagr_test.m4a
+    fetch(fullhostname()+"/wypadek",
+        { method: "POST",
+            headers: headers,
+            body: JSON.stringify({id: 1, name: "Jan", surname: "Kowalski", desc: "Przy parkowaniu mój samochód uderzył w słup.",
+                audioData: "test" })}
+    ).then(response => {
+        console.log(response);
+        res.status(200).send({message: "Test completed", response: response});
+    }
+    ).catch(err => {
+        console.log(err);
+        res.status(500).send({message: "Error during test"});
+    });
+});
+
 app.post("/wypadek", (req, res) => {
-  const {id, name, surname, desc, audioData } = req.body;
+  let {id, name, surname, desc, audioData } = req.body;
   const sql = `INSERT INTO wypadek (id, name, surname, desc, audioData) VALUES (?, ?, ?, ?, ?)`;
   connection.query(sql, [id, name, surname, desc, audioData], (err, result) => {
 
@@ -54,6 +80,10 @@ app.post("/wypadek", (req, res) => {
             console.log(err);
             res.status(500).send("Error saving the data");
         } else {
+            if(audioData == "test"){
+                //stream from audio file ./nagr_test.m4a
+                audioData = fs.createReadStream("./nagr_test.m4a");
+            }else{
             if(audioData && audioData.length > 0)
             {
                 //try to convert audio to text
@@ -61,12 +91,12 @@ app.post("/wypadek", (req, res) => {
                     file: audioData,
                     model: "whisper-1",
                 });
-
-
+                console.log("chat has transcribed audio: "+transcripton);
 
             }
+            }
         }
-      let category = "unknown";
+      let category = "nieznane";
         const categories = [
             "wstrząs mózgu",
             "złamanie otwarte",
@@ -74,24 +104,35 @@ app.post("/wypadek", (req, res) => {
             "niedrożność dróg oddechowych",
             "amputacja",
             "rana",
-            "środowiskowe"
+            "środowiskowe",
+            "wypadek komunikacyjny"
         ];
         const stringOfCategories = categories.join(", ");
         //use chatgpt to categorize the description of the accident
         const response = openai.api().chatCompletion.create({
             model: "gpt-3.5-turbo",
             messages: [
-                {role: "system", content: "You will get two descriptions of the accident (secound one is from speech to text). Please categorize the accident described in those descriptions. Available categories are: "+stringOfCategories},
+                {role: "system", content: "You will get two descriptions of the accident (secound one is from speech to text). Please categorize the accident described in those descriptions. Available categories are: "+stringOfCategories+"; Description1 : "+desc+"; Description2 : "+transcripton},
                 {role: "user", content: desc},
             ],
         });
         //if the chat response is a category, save it
        if(categories.includes(response.choices[0].message.content)){
-
+           category = response.choices[0].message.content;
        }else{
-           category = "unknown";
+           category = "nieznane";
        }
       res.status(201).send({message: "Data saved successfully", category: category});
-  });
-
+    }
+    );
 }
+);
+
+//error handling
+app.use((req, res) => {
+    res.status(404).send("Błąd 404 - Endpoint not found");
+});
+
+app.listen(process.env.PORT || host.port, () => {
+    console.log("Server is running");
+});
